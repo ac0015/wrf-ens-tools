e391a3d57102b76bad1c70bec85418990cb6b123
Austin Coleman <aucolema@compute-8-5.localdomain>
2018-08-28 08:15:04 -0500

    Modified verif_analysis

--- verif_analysis.py
+++ verif_analysis.py
@@ -11,13 +11,6 @@ from os import chdir
 from datetime import datetime, timedelta
 
 ncfiles = ['/lustre/research/bancell/aucolema/HWT2016runs/2016051300/fssfinal.nc']
-# For second plot
-sens_var = ['300_hPa_GPH', '300_hPa_T', '300_hPa_U-Wind', '300_hPa_V-Wind',
-             '500_hPa_GPH', '500_hPa_T', '500_hPa_U-Wind', '500_hPa_V-Wind',
-             '700_hPa_GPH', '700_hPa_T', '700_hPa_U-Wind', '700_hPa_V-Wind',
-             '850_hPa_GPH', '850_hPa_T', '850_hPa_U-Wind', '850_hPa_V-Wind',
-             '925_hPa_T', '925_hPa_U-Wind', '925_hPa_V-Wind',
-             'SLP', '2m_Temp', '2m_Dewpt', '10m_U-Wind', '10m_V-Wind']
 
 # Plot FSS for subset size/pperf sigma
 fullensnum = 42
@@ -65,10 +58,9 @@ plt.close()
 dat.close()
 
 # Plot FSS for each type of subset with subset size
-if len(sens_var) > 6:
-    figpath = 'fss_subset_size_sigma{}_{}.png'.format(sigma, 'all')
-else:
-    figpath = 'fss_subset_size_sigma{}_{}.png'.format(sigma, '-'.join(str(var) for var in sens_var))
+uhthresholds = [40]
+nbrhds = [30]
+figpath = 'fss_subset_size_sigma{}_nbr{}_uh{}.png'.format(sigma, nbrhds[0], uhthresholds[0])
 plt.figure(2, figsize=(10,10))
 for file in ncfiles:
     dat = Dataset(file)
@@ -87,65 +79,87 @@ for file in ncfiles:
     fens_rbox = dat.variables['Full_Ens_FSS_Rbox'][inds]
     fss_all = dat.variables['Subset_FSS_Total'][inds]
     fss_rbox = dat.variables['Subset_FSS_Rbox'][inds]
-    sens_mask = []
-    for var in sensvars:
-        #print(list(var), list(sens_var))
-        if list(var) != list(sens_var):
-           sens_mask.append(False)
-        else:
-           sens_mask.append(True)
+    sens_vars = [list(x) for x in set(tuple(x) for x in sensvars)]
+    colors_rbox = ['darkorange', 'maroon', 'purple', 'midnightblue', 
+                  'darkolivegreen', 'seagreen', 'red']
+    colors_all = ['coral', 'indianred', 'mediumorchid', 'royalblue',
+                   'olive', 'mediumseagreen', 'orangered']
     senstimes = dat.variables['Sens_Time'][inds]
     nbrs = dat.variables['Neighborhood'][inds]
-    for method in np.unique(methods):
-        for percent in np.unique(percents):
-            for stime in np.unique(senstimes):
-                for nbr in np.unique(nbrs):
-                    for uh_thresh in np.unique(uhthresh):
-                        x = subsize[sens_mask]
-                        #print(np.shape(fss_all))
-                        fss_all_tmp = fss_all[sens_mask]
-                        fss_rbox_tmp = fss_rbox[sens_mask]
-                        sub_inds = np.where((methods[sens_mask] == method) & (percents[sens_mask] == percent) \
-                                            & (senstimes[sens_mask] == stime) & (nbrs[sens_mask] == nbr) \
-                                            & (uhthresh[sens_mask] == uh_thresh))
-                        x = x[sub_inds]
-                        y_all = fss_all_tmp[sub_inds]
-                        y_rbox = fss_rbox_tmp[sub_inds]
-                        if (method == 'percent') and (percent == 0.):
-                            print('We have a winner!')
-                            color_all = 'darkgray'
-                            color_rbox = 'grey'
-                            lw = 1
-                            zorder = 10
-                        else:
-                            color_all = 'lightgreen'
-                            color_rbox = 'purple'
-                            lw = 1
-                            zorder = 8
-                        plt.plot(x[x<fullensnum], y_all[x<fullensnum], 'd', c=color_all, ls='-', 
-                                 linewidth=lw, zorder=zorder)
-                        plt.plot(x[x<fullensnum], y_rbox[x<fullensnum], 'd', c=color_rbox, ls='-', 
-                                 linewidth=lw, zorder=zorder)
-                        print(method, percent, stime, nbr, uhthresh[sub_inds], sig[sub_inds], y_all, y_rbox)
-                        del sub_inds
-                        del x
-                        del y_all
-                        del y_rbox
-                    
-    plt.plot(np.unique(subsize[subsize<fullensnum]), 
-             np.ones_like(np.unique(subsize[subsize<fullensnum]))*np.mean(fens_all), ls='--',
-                             linewidth=2., c='maroon', label='Full Ensemble Mean Total Domain FSS')
-    plt.plot(np.unique(subsize[subsize<fullensnum]),  
-             np.ones_like(np.unique(subsize[subsize<fullensnum]))*np.mean(fens_rbox), ls='--',
-                             linewidth=2., c='blue', label='Full Ensemble Mean Response Box FSS')
-plt.legend()
+    i = 0
+    for sens_var in sens_vars:
+        print(sens_var)
+        color_all = colors_all[i]
+        color_rbox = colors_rbox[i]
+        lw = 1
+        if ('500_hPa_GPH' in sens_var) and ('SLP' in sens_var):
+            label_all = "FSS Tot for All Sens Vars"
+            label_rbox = "FSS Rbox for All Sens Vars"
+        else:
+            label_all="FSS Tot for Sens Vars: {}".format(' '.join(str(var) for var in sens_var))
+            label_rbox="FSS RBox for Sens Vars: {}".format(' '.join(str(var) for var in sens_var))
+        plt.plot(0, 0, color=colors_all[i], label=label_all)
+        plt.plot(0, 0, color=colors_rbox[i], label=label_rbox)
+        sens_mask = []
+        for k in range(len(sensvars)):
+            if list(sensvars[k]) == list(sens_var):
+                sens_mask.append(True)
+            else:
+                sens_mask.append(False)
+        for method in np.unique(methods):
+            for percent in np.unique(percents):
+                for stime in np.unique(senstimes):
+                    for nbr in nbrhds:
+                        for uh_thresh in uhthresholds:
+                            x = subsize[sens_mask]
+                            #print(np.shape(fss_all))
+                            fss_all_tmp = fss_all[sens_mask]
+                            fss_rbox_tmp = fss_rbox[sens_mask]
+                            sub_inds = np.where((methods[sens_mask] == method) & (percents[sens_mask] == percent) \
+                                                & (senstimes[sens_mask] == stime) & (nbrs[sens_mask] == nbr) \
+                                                & (uhthresh[sens_mask] == uh_thresh))
+                            x = x[sub_inds]
+                            y_all = fss_all_tmp[sub_inds]
+                            y_rbox = fss_rbox_tmp[sub_inds]
+                            if (method == 'percent') and (percent == 0.):
+                                print('We have a winner!')
+                                color_all = 'darkgray'
+                                color_rbox = 'k'
+                                lw = 3.
+                                zorder = 10
+                                alpha = 0.9
+                            else:
+                                color_all = colors_all[i]
+                                color_rbox = colors_rbox[i]
+                                zorder = 8
+                                lw = 1.
+                                alpha = 0.5
+                            plt.plot(x[x<fullensnum], y_all[x<fullensnum], 'd', c=color_all, ls='-', 
+                                     linewidth=lw, zorder=zorder, alpha=alpha)
+                            plt.plot(x[x<fullensnum], y_rbox[x<fullensnum], 'd', c=color_rbox, ls='-', 
+                                     linewidth=lw, zorder=zorder, alpha=alpha)
+                            #print(method, percent, stime, nbr, uhthresh[sub_inds], sig[sub_inds], y_all, y_rbox)
+                            del sub_inds
+                            del x
+                            del y_all
+                            del y_rbox
+        i += 1 
+    x = np.unique(subsize[subsize<fullensnum])         
+    fens_all_masked = fens_all[(uhthresh == uhthresholds[0]) & (nbrs == nbrhds[0])]
+    print(np.min(fens_all_masked), np.max(fens_all_masked))         
+    plt.plot(x, np.ones_like(x)*np.mean(fens_all_masked), ls='--',
+                             linewidth=2., c='maroon', label='Full Ensemble Total Domain FSS')
+    plt.plot(x, np.ones_like(x)*np.mean(fens_rbox[(uhthresh == uhthresholds[0]) & (nbrs == nbrhds[0])]), ls='--',
+                             linewidth=2., c='blue', label='Full Ensemble Response Box FSS')
+l = plt.legend(fontsize=5)
+l.set_zorder(13)
 if len(list(sens_var)) > 6:
     sensstr = 'All'
 else:
     sensstr = ', '.join(str(var).replace('_', ' ') for var in list(sens_var))
 plt.title(r'Fractional Skill Scores of Subsets from {} UTC run valid f{} with $\sigma$ = {}'.format(str(init[0]), 
-          str(rtime[0]), str(sigma)) + \
-         "\nSensitivity Variables: {}".format(sensstr),fontsize=11)
+          str(rtime[0]), str(sigma)) + '\n' + \
+         r"UH Threshold = {} m$^2$/s$^2$; Neighborhood = {} km".format(uhthresholds[0], nbrhds[0]),fontsize=11)
 plt.xlabel('Subset Size')
 plt.ylabel('FSS')
 plt.grid()
