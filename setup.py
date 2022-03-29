0a4f9b4ecde4071679b5df5388af4f056ff64b2b
Austin Coleman <austin.coleman@ttu.edu>
2022-03-29 16:40:34 -0500

    Got rid of pyproj requirement - breaks installation.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='wrf-ens-tools',
     version='0.1',
-    install_requires=['netcdf4', 'numpy', 'scipy', 'xarray', 'pandas', 'pyproj<3.0',
+    install_requires=['netcdf4', 'numpy', 'scipy', 'xarray', 'pandas',
                       'wrf-python', 'matplotlib', 'siphon', 'cartopy', 'sklearn',
                       'dask', 'metpy>=1.0', 'cmocean'],
     license='BSD-3',
